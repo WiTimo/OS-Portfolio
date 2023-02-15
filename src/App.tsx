c0eea3714e599410d58393946ba8fce20f73197c
Timo Wilde <timowilde2007@gmail.com>
2023-02-15 19:11:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import LoginPage from './pages/LoginPage.page'
+
+function App() {
+
+  return (
+    <div className="App">
+        <LoginPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/LoginPage.page.tsx
@@ -0,0 +1,14 @@
+import "./LoginPage.style.scss";
+
+export default function LoginPage() {
+    return(
+        <div className="login-page-container">
+            <div className="login-page-section-top">
+                <p>Hallo</p>
+            </div>
+            <div className="login-page-section-bottom">
+                <p>Welt</p>
+            </div>
+        </div>
+    )
+}
